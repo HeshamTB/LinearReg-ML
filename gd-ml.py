694b49ada8ca27866c98fe28d1f32ca52ef784d6
HeshamTB <hishaminv@gmail.com>
2021-10-16 15:05:35 +0300

    proper vectorized code
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

--- gd-ml.py
+++ gd-ml.py
@@ -1,21 +1,25 @@
 import sys
+
+import numpy
 import numpy as np
+
 np.set_printoptions(threshold=sys.maxsize)
 
+
 def main():
-    trainData = readCSVFile('train.csv') # Contains price labels
+    trainData = readCSVFile('train.csv')  # Contains price labels
     yTrain = readCSVFile('train.csv')
     testData = readCSVFile('test.csv')
-    cols1 = [1, 4, 17, 18, 19, 43, 44, 46, 62, 77] # Features selected
+    cols1 = [1, 4, 17, 18, 19, 43, 44, 46, 62, 77]  # Features selected
     selectedColumns(testData, cols1)
     selectedColumns(trainData, cols1)
     selectedColumns(yTrain, [80])
-    #for row in trainData: print(row)
+    # for row in trainData: print(row)
     trainData = toNumpyMat(trainData)
     testData = toNumpyMat(testData)
     yTrain = toNumpyMat(yTrain)
     lr = 0.0001
-    #for row in testData: print(row)
+    # for row in testData: print(row)
     # use Sum of Squared diff SSD for cost func
     # Goal is to fit line/s over the train data and get the minimum error (Cost function value) using test data.
     # We try to automate this process by changing lr and selecting the best fit.
@@ -23,33 +27,45 @@ def main():
     # for gradiant  [hypo - (actual y)]
 
     theta = np.random.rand(len(cols1))
-    trainData.transpose()
-    print(trainData)
-    #np.vstack([np.ones((1,len(trainData))), trainData])
+#    print(trainData)
+#    print(theta)
+    # trainData.transpose()
+    # print(trainData)
+    # np.vstack([np.ones((1,len(trainData))), trainData])
+    Y = hypLinear(trainData, theta.transpose())
+#    print(Y)
     # for example in trainData:
     #     example = np.matrix(example).transpose()
     #     example = np.vstack([np.ones(1), example])
+    #     # print(example)
     #     Y = hypLinear(np.matrix(theta), example)
+    #     # print(Y)
     return 0
 
-def hypLinear(XTrain, theta):
-    return (theta*XTrain).sum()
+
+def hypLinear(XTrain, theta) -> numpy.ndarray:
+    return theta * XTrain
+
 
 def printFeatureIndcies(data):
-    for i,val in enumerate(data[0]):
-        print((i,val))
+    for i, val in enumerate(data[0]):
+        print((i, val))
 
-def readCSVFile(filename : str) -> list:
+
+def readCSVFile(filename: str) -> list:
     import csv
     data = list()
     with open(filename, 'r') as csvFile:
         csvReader = csv.reader(csvFile)
-        for i,row in enumerate(csvReader):
-            if i == 0: continue
-            else: data.append(row)
+        for i, row in enumerate(csvReader):
+            if i == 0:
+                continue
+            else:
+                data.append(row)
     return data
 
-def selectedColumns(data:list, columns:list):
+
+def selectedColumns(data: list, columns: list):
     # Remove data not included in study
     # Need Cols 1, 4, 17, 18, 19, 43, 44, 46, 62, 77
     # This index base includes ID col.
@@ -65,9 +81,12 @@ def selectedColumns(data:list, columns:list):
         # Replace new row
         data[rowIndex] = newRow
 
+
 def toNumpyMat(data):
     return np.array(data)
 
     pass
+
+
 if __name__ == '__main__':
     exit(main())
