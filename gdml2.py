b3b6733986820a713829ccf7c10830556e4d2041
HeshamTB <hishaminv@gmail.com>
2021-10-17 22:55:26 +0300

    working stuff

--- gdml2.py
+++ gdml2.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 import csv
 
-
 np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -17,6 +16,7 @@ def main():
         trainX = trainX_full
         lr = float(sys.argv[2])
         selectedColumns(trainX, cols1)
+        trainX = raiseOrder(trainX, 2)
         trainX = featurescaling(trainX)
         trainX = np.matrix(trainX)
         trainX = trainX.transpose()
@@ -30,74 +30,94 @@ def main():
         trainY = trainY.transpose()
         # print('trainY ', trainY.shape)
         theta = np.random.random(trainX.shape[0])
-        theta = np.matrix(theta)*10
+        theta = np.matrix(theta) * 10
         # print('theta ', theta.shape)
         loss, theta = fit(lr, theta, trainX, trainY, True, int(sys.argv[3]))
-        #print(loss, theta)
+        # print(loss, theta)
     elif sys.argv[1] == 'test':
         testX = readCSVFile('test.csv')
-        #printFeatureIndcies(testX)
+        # printFeatureIndcies(testX)
         selectedColumns(testX, cols1)
         testX = np.matrix(testX)
         testX = testX.transpose()
         ones = np.ones(testX.shape[1])
         testX = np.vstack([ones, testX])  # Now in shape and intercept ones
-
         testY = readCSVFile('test.csv')
         selectedColumns(testY, [80])
         testY = np.matrix(testY)
         testY = testY.transpose()
         theta = list()
-        for i in range(2,13): theta.append(float(sys.argv[i]))
-        Y_infer = hypo(testX, theta)
-        loss_test = ((Y_infer - testY).sum() ** 2) / testX.shape[1]
+        for i in range(2, 13): theta.append(float(sys.argv[i]))
+        loss_test = test(testX, testY, theta)
         print(loss_test, theta)
+
+
+def test(testX, testY, theta):
+    Y_infer = hypo(testX, theta)
+    loss_test = ((Y_infer - testY).sum() ** 2) / testX.shape[1]
+    return loss_test
 
 
 def fit(lr, theta, trainX, trainY, verbose, iterations=500000):
     count = 0
     min = math.inf
+    loss = 0
     # data = list()
     while count < iterations:
-        Y = hypo(trainX, theta)
-        # print(Y)
-        # print('Y ', Y.shape)
-        # print(trainX.shape[1])
-        dJ = (trainX * (Y - trainY).transpose()) / trainX.shape[1]
-        #print('dJ ', dJ.shape)
-        theta = theta - lr * dJ.transpose()
-        loss = ((Y - trainY).sum() ** 2) / trainX.shape[1]
-        # if loss < min: min = loss; data.append([count, loss, theta])
-        count += 1
-        if loss == math.nan or loss == math.inf: break  # Diverged
-        if count % 100 == 0 and verbose: print('i=%d, loss=%f, theta=%s' % (count, loss, theta))
+        try:
+            Y = hypo(trainX, theta)
+            # print(Y)
+            # print('Y ', Y.shape)
+            # print(trainX.shape[1])
+            dJ = (trainX * (Y - trainY).transpose()) / trainX.shape[1]
+            # print('dJ ', dJ.shape)
+            theta = theta - lr * dJ.transpose()
+            loss = ((Y - trainY).sum() ** 2) / trainX.shape[1]
+            # if loss < min: min = loss; data.append([count, loss, theta])
+            count += 1
+            if loss == math.nan or loss == math.inf: break  # Diverged
+            if count % 100 == 0 and verbose: print('i=%d, loss=%f, theta=%s' % (count, loss, theta))
+        except KeyboardInterrupt:
+            break
     # return data
     return loss, theta
 
 
+def raiseOrder(data: list[list], order: int):
+    data_new = list()
+    for i, row in enumerate(data):
+        new_row = list()
+        for j, elemnt in enumerate(row):
+            for k in range(order):
+                new_row.append(elemnt ** (k+1))
+        data_new.append(new_row)
+    return data_new
+
 def hypo(x, theta):
     return theta * x
 
+
 def featurescaling(array):
     #  feature scaling
-    biggest_value=0
-    counter_list=0
-    counter_element=0
+    biggest_value = 0
+    counter_list = 0
+    counter_element = 0
     for mnm in range(len(array[0])):
         for me in array:
-            if (int (array[counter_list][counter_element]))>biggest_value:
-                biggest_value=(int (array[counter_list][counter_element]))
-            counter_list=counter_list+1
-        counter_list=0
+            if (int(array[counter_list][counter_element])) > biggest_value:
+                biggest_value = (int(array[counter_list][counter_element]))
+            counter_list = counter_list + 1
+        counter_list = 0
 
         for mee in array:
-            array[counter_list][counter_element]=  ((int (array[counter_list][counter_element]))/biggest_value)
-            counter_list=counter_list+1
+            array[counter_list][counter_element] = ((int(array[counter_list][counter_element])) / biggest_value)
+            counter_list = counter_list + 1
 
-        counter_list=0
-        biggest_value=0
-        counter_element=counter_element+1
+        counter_list = 0
+        biggest_value = 0
+        counter_element = counter_element + 1
     return array
+
 
 def readCSVFile(filename: str) -> list:
     import csv
